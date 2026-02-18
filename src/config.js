1f5188d79357014d84ea62994cbdcff73134fda3
hugstokens-stack <hugstokens@gmail.com>
2026-02-18 11:31:49 -0500

    Add game configuration for HUGS token settings

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+  HUGS: {
+    tokenAddress: '0xYourTokenAddress',
+    decimals: 18,
+    symbol: 'HUGS',
+  },
+  gameSettings: {
+    maxPlayers: 4,
+    timeLimit: 300,
+  },
+};
+
+export default config;
